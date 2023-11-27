e304888badc55b474e43fbed55c0068ec7e3fbf6
adxo777 <adammosref@gmail.com>
2023-11-27 19:55:35 +0100

    addmenu

--- app/src/main/java/com/example/tplogin/MainActivity.kt
+++ app/src/main/java/com/example/tplogin/MainActivity.kt
@@ -3,17 +3,29 @@ package com.example.tplogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.tplogin.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 class MainActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
     lateinit var user: FirebaseUser
+
+    // menu
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
+    lateinit var actionToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initMenu(binding)
+
+
         auth = FirebaseAuth.getInstance()
         user = auth.currentUser!!
         if (user == null) {
@@ -29,5 +41,21 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun initMenu(binding: ActivityMainBinding) {
+
+            drawerLayout = binding.drawerLayout
+            navView = binding.navView
+
+            actionToggle = ActionBarDrawerToggle(this, drawerLayout, binding.toolbar, 0, 0)
+            drawerLayout.addDrawerListener(actionToggle)
+
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+//make icon menu visible
+            actionToggle.syncState()
+
+
     }
 }
